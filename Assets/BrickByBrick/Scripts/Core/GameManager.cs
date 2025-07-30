12967c0428e2a685b5f50c86d97860b2bae434bf
Bedrockbreaker <coolmandude101@gmail.com>
2025-07-30 16:40:47 -0700

    Fix game object destroying itself on scene transitions

--- Assets/BrickByBrick/Scripts/Core/GameManager.cs
+++ Assets/BrickByBrick/Scripts/Core/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
