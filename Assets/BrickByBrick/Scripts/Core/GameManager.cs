5bf049e3cdb2fb78a14c75c8043ca3b89c28340f
Bedrockbreaker <coolmandude101@gmail.com>
2025-07-30 15:44:52 -0700

    Add game manager singleton

---
+++ Assets/BrickByBrick/Scripts/Core/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+	public void Awake()
+	{
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+}
